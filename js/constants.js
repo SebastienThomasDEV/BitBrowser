e141d099b2733d54292351de343b6c463824c890
Sébastien THOMAS <74055963+SebastienThomasDEV@users.noreply.github.com>
2023-08-29 15:23:09 +0200

    restart from scratch

---
+++ js/constants.js
@@ -0,0 +1,21 @@
+export const CRYPTOS = [
+    "BTC",
+    "ETH",
+    "XRP",
+    "BNB",
+    'USDT',
+    'USDC',
+    'ADA',
+    'DOGE',
+    'SOL',
+    'DOT',
+    'DAI',
+    'TRX'
+]
+
+export const DEVISES = [
+    "EUR",
+    "USD",
+    "GBP",
+    "JPY",
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,105 +1,51 @@
+import { CRYPTOS, DEVISES } from "./constants.js";
+
+let add = document.getElementById("add_btn");
+let datas = {
+    labels : [],
 
 
-async function getData() {
-    let response1 = await fetch('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR')
-    let data_graph = [response1.json()]
-    return data_graph
 }
+add.addEventListener("click", async function () {
+    const { value: PAIR } = await Swal.fire({
+        title: 'Choisissez votre crypto et votre devise',
+        html:
+            `
+            <select id="swal-input-crypto" class="swal2-input">
+                ${CRYPTOS.map(crypto => `<option value="${crypto}">${crypto}</option>`)}
+            </select>
+            <select id="swal-input-devise" class="swal2-input">
+                ${DEVISES.map(devise => `<option value="${devise}">${devise}</option>`)} 
+            </select>
+            `,
+        focusConfirm: false,
+        preConfirm: () => {
+            return [
+                document.getElementById('swal-input-crypto').value,
+                document.getElementById('swal-input-devise').value
+            ]
+        }
+    })
 
-async function addData(chart) {
-    let new_data = await getData()
-    for (let i = 0; i< new_data.length; i++) {
-        let fresh = await new_data[i]
-        chart.data.datasets[i].data.push(fresh["EUR"])
+    if (PAIR) {
+        const { value: COLOR } = await Swal.fire({
+            title: 'Choisissez la couleur de votre graphique',
+            html:
+                `
+            <input id="swal-input-color-border" type="color"><p>Couleur de la bordure</p>
+            <input id="swal-input-color-bg" type="color"><p>Couleur de fond</p>
+            `,
+            focusConfirm: false,
+            preConfirm: () => {
+                return [
+                    document.getElementById('swal-input-color-border').value,
+                    document.getElementById('swal-input-color-bg').value
+                ]
+            }
+        })
+        if (COLOR) {
+            Swal.fire(JSON.stringify(COLOR))
+            console.log(COLOR, PAIR)
+        }
     }
-    let date = new Date;
-    chart.data.labels.push(`${date.getHours()}h${date.getMinutes()}`)
-    chart.update();
-}
-
-function alea(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-function build_graph(nb_graph) {
-    const graph = document.createElement("canvas"); //créer un element HTML => canvas
-    graph.setAttribute('id',`Chart${nb_graph}`); //donne un id à l'element HTML => id incrémentable
-    const choice = "BTC"
-    const config = config_graph(choice)
-    const builded = new Chart(graph,config);
-    $('.add_crypto').append(graph);
-    setInterval(() => {
-        addData(builded);}, 10000);
-}
-  
-function config_graph(choice) {
-    let color = [alea(0, 255),alea(0, 255),alea(0, 255)]
-    const labels = []
-    const data = {
-        labels: labels,
-        datasets: [{
-            label: `${choice}/EUR`,
-            backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})`,
-            borderColor: `rgb(${color[0]},${color[1]},${color[2]})`,
-            data: [],
-        }]
-    }
-    
-    const config = {
-        type: 'line',
-        data: data,
-        options: {}
-    }
-    
-    return config
-}
-
-
-
-
-
-
-const start_btn = document.querySelector(".start");
-const close_btn = document.querySelector(".close");
-const add_btn = document.querySelector(".add_btn");
-let nb_graph = 0
-
-add_btn.addEventListener('click', () => {
-    build_graph(nb_graph)
-    nb_graph++
 })
-
-$(".close").hide();
-$(".graph_wpr").hide();
-$(".add_crypto").hide();
-$(".page_title").hide();
-
-
-
-start_btn.addEventListener('click', () => {
-    $(".close").show();
-    $(".start").hide()
-    $(".page_title").show();
-    $(".description").hide();
-    $(".add_crypto").show();
-})
-
-close_btn.addEventListener('click', () => {
-    $(".start").show()
-    $(".close").hide();
-    $(".add_crypto").hide();
-    $(".description").show();
-    $(".page_title").hide();
-})
-
-let crypto = [];
-let choice = '';
-let url = `https://min-api.cryptocompare.com/data/price?fsym=${choice}&tsyms=EUR;`;
-
-
-
-    
-
-
-
-
